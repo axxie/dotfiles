161218689e24a2b06d73cdc0f788b3fcb03674cf
Axxie J <axxie@users.noreply.github.com>
2019-10-13 09:22:15 +0300

    add checking mandatory requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,45 @@
-#!/bin/sh
+#!/bin/sh -e
+
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+    echo ${RED}"Error: $@"${RESET} >&2
+}
+
+setup_color() {
+    # Only use colors if connected to a terminal
+    if [ -t 1 ]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
+    fi
+}
+
+setup_color
+
+for command in wget python git
+do
+    command_exists "$command" || {
+        error "Required command \"$command\" is not installed"
+        missing_requirement=yes
+    }
+done
+
+if [ -n "$missing_requirement" ]; then
+    exit 1
+fi
 
 echo Bootstrapping...
 
