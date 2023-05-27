645278b8c2d424e9bb562d69bef76cc2546ee664
Axxie J <axxie@users.noreply.github.com>
2023-05-27 14:10:40 +0300

    set height and add title to ctrl+t menu

--- roles/zsh/files/fzf-settings.plugin.zsh
+++ roles/zsh/files/fzf-settings.plugin.zsh
@@ -1,3 +1,7 @@
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+  --color header:italic
+  --header '<tab> to select, <enter> to finish, <ctrl-/> to change preview'
+  --height 70%"
+
